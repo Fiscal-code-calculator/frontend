8d3b9c4b57d006b0f7c79cc411ba74f3317c276c
gkkconan <conan.gkk@gmail.com>
2024-08-03 17:07:23 +0200

    chore(app-routing.module.ts): update paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import {NgModule} from "@angular/core";
-import {RouterModule,Routes} from "@angular/router";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from "./components/authentication/login/login.component";
+import { RegisterComponent } from "./components/authentication/register/register.component";
+import { CalculatorComponent } from "./components/calculator/calculator.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: CalculatorComponent, canActivate: [AuthGuard] },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+
+];
 
 @NgModule({
   imports:[RouterModule.forRoot(routes)],
   exports:[RouterModule]
 })
 
-export class AppRoutingModule{}
+export class AppRoutingModule{ }
