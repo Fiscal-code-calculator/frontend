a70b223b2f0fdc5886818357ef8eebe0512fe89a
antoniotassone94 <antonio.tassone@studio.unibo.it>
2024-08-08 15:05:57 +0200

    fix error in the guard condition to redirect homepage if the user isn't logged.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,21 @@
 import { inject } from "@angular/core";
-import { CanActivateFn,CanActivateChildFn } from "@angular/router";
+import { CanActivateFn,CanActivateChildFn, Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
 
 export const authGuardParent: CanActivateFn = (route, state) => {
 	const authService: AuthService = inject(AuthService);
-	return authService.isAuthenticated();
+	const router:Router = inject(Router);
+	if(authService.isAuthenticated() === false){
+		return router.parseUrl("/");
+	}
+	return true;
 };
 
 export const authGuardChildren: CanActivateChildFn = (route, state) => {
 	const authService: AuthService = inject(AuthService);
-	return authService.isAuthenticated();
+	const router:Router = inject(Router);
+	if(authService.isAuthenticated() === false){
+		return router.parseUrl("/");
+	}
+	return true;
 };
